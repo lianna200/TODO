4de79f85b94c58a3a4a13a569026b51dd4c762ba
lianna200 <lianna200@gmail.com>
2017-08-12 00:19:22 -0400

    Update delete_todo.php

--- delete_todo.php
+++ delete_todo.php
@@ -1,20 +1,20 @@
 <?php
 // Get ID
-$category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
+$todo_id = filter_input(INPUT_POST, 'todo_id', FILTER_VALIDATE_INT);
 // Validate inputs
-if ($category_id == null || $category_id == false) {
-    $error = "Invalid category ID.";
+if ($todo_id == null || $todo_id == false) {
+    $error = "Invalid TO DO Id.";
         include('error.php');
 	} else {
 	    require_once('database.php');
 	        // Add the product to the database  
-		    $query = 'DELETE FROM categories 
-		                  WHERE categoryID = :category_id';
+		    $query = 'DELETE FROM todo 
+		                  WHERE todoID = :todo_id';
 				      $statement = $db->prepare($query);
-			  $statement->bindValue(':category_id',
-			  $category_id);
+			  $statement->bindValue(':todo_id',
+			  $todo_id);
 			$statement->execute();
 		     $statement->closeCursor();
 // Display the Category	  List page
-include('category_list.php');							  }
+include('todo_list.php');							  }
 ?>
