7399050394ff15529a57720ccd74a6db9d672805
lianna200 <lianna200@gmail.com>
2017-08-11 22:56:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,38 +2,38 @@
 require_once('database.php');
 
 // Get category ID
-if (!isset($category_id)) {
-    $category_id = filter_input(INPUT_GET, 'category_id', 
+if (!isset($todo_id)) {
+    $todo_id = filter_input(INPUT_GET, 'todo_id', 
             FILTER_VALIDATE_INT);
-    if ($category_id == NULL || $category_id == FALSE) {
-        $category_id = 1;
+    if ($todo_id == NULL || $todo_id == FALSE) {
+        $todo_id = 1;
     }
 }
 // Get name for selected category
-$queryCategory = 'SELECT * FROM categories
-                  WHERE categoryID = :category_id';
-$statement1 = $db->prepare($queryCategory);
-$statement1->bindValue(':category_id', $category_id);
+$queryTodo = 'SELECT * FROM todo
+                  WHERE todoID = :todo_id';
+$statement1 = $db->prepare($queryTodo);
+$statement1->bindValue(':todo_id', $todo_id);
 $statement1->execute();
-$category = $statement1->fetch();
-$category_name = $category['categoryName'];
+$todo = $statement1->fetch();
+$todo_name = $todo['todoName'];
 $statement1->closeCursor();
 
 
 // Get all categories
-$query = 'SELECT * FROM categories
-                       ORDER BY categoryID';
+$query = 'SELECT * FROM todo
+                       ORDER BY todoID';
 $statement = $db->prepare($query);
 $statement->execute();
 $categories = $statement->fetchAll();
 $statement->closeCursor();
 
 // Get products for selected category
-$queryProducts = 'SELECT * FROM products
-                  WHERE categoryID = :category_id
-                  ORDER BY productID';
-$statement3 = $db->prepare($queryProducts);
-$statement3->bindValue(':category_id', $category_id);
+$queryTask = 'SELECT * FROM task
+                  WHERE todoID = :todo_id
+                  ORDER BY taskID';
+$statement3 = $db->prepare($queryTask);
+$statement3->bindValue(':todo_id', $todo_id);
 $statement3->execute();
 $products = $statement3->fetchAll();
 $statement3->closeCursor();
@@ -43,24 +43,24 @@ $statement3->closeCursor();
 
 <!-- the head section -->
 <head>
-    <title>My Guitar Shop</title>
+    <title>My Task List</title>
     <link rel="stylesheet" type="text/css" href="main.css" />
 </head>
 
 <!-- the body section -->
 <body>
-<header><h1>Product Manager</h1></header>
+<header><h1>To DO List Manager</h1></header>
 <main>
-    <h1>Product List</h1>
+    <h1>Task List</h1>
 
     <aside>
         <!-- display a list of categories -->
-        <h2>Categories</h2>
+        <h2>ToDos</h2>
         <nav>
         <ul>
-            <?php foreach ($categories as $category) : ?>
-            <li><a href=".?category_id=<?php echo $category['categoryID']; ?>">
-                    <?php echo $category['categoryName']; ?>
+            <?php foreach ($tasks as $task) : ?>
+            <li><a href=".?task_id=<?php echo $task['taskID']; ?>">
+                    <?php echo $task['taskName']; ?>
                 </a>
             </li>
             <?php endforeach; ?>
@@ -70,36 +70,35 @@ $statement3->closeCursor();
 
     <section>
         <!-- display a table of products -->
-        <h2><?php echo $category_name; ?></h2>
+        <h2><?php echo $todo_name; ?></h2>
         <table>
             <tr>
-                <th>Code</th>
+               
                 <th>Name</th>
-                <th class="right">Price</th>
                 <th>&nbsp;</th>
             </tr>
 
-            <?php foreach ($products as $product) : ?>
+            <?php foreach ($tasks as $task) : ?>
             <tr>
-                <td><?php echo $product['productCode']; ?></td>
-                <td><?php echo $product['productName']; ?></td>
-                <td class="right"><?php echo $product['listPrice']; ?></td>
-                <td><form action="delete_product.php" method="post">
-                    <input type="hidden" name="product_id"
-                           value="<?php echo $product['productID']; ?>">
-                    <input type="hidden" name="category_id"
-                           value="<?php echo $product['categoryID']; ?>">
+            
+                <td><?php echo $task['taskName']; ?></td>
+          
+                <td><form action="delete_task.php" method="post">
+                    <input type="hidden" name="task_id"
+                           value="<?php echo $task['taskID']; ?>">
+                    <input type="hidden" name="todo_id"
+                           value="<?php echo $task['todoID']; ?>">
                     <input type="submit" value="Delete">
                 </form></td>
             </tr>
             <?php endforeach; ?>
         </table>
-        <p><a href="add_product_form.php">Add Product</a></p>
-        <p><a href="category_list.php">List Categories</a></p>        
+        <p><a href="add_task_form.php">Add Task</a></p>
+        <p><a href="category_list.php">List TO DOs</a></p>        
     </section>
 </main>
 <footer>
-    <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
+    <p>&copy; <?php echo date("Y"); ?> My TO Do</p>
 </footer>
 </body>
 </html>
