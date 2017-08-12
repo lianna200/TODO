670f59d87de7661a39a89515e435b5a65f67978a
lianna200 <lianna200@gmail.com>
2017-08-11 23:20:41 -0400

    Update add_task_form.php

--- add_task_form.php
+++ add_task_form.php
@@ -1,8 +1,8 @@
 <?php
 require('database.php');
 $query = 'SELECT *
-          FROM categories
-          ORDER BY categoryID';
+          FROM todo
+          ORDER BY todoID';
 $statement = $db->prepare($query);
 $statement->execute();
 $categories = $statement->fetchAll();
@@ -13,45 +13,43 @@ $statement->closeCursor();
 
 <!-- the head section -->
 <head>
-    <title>My Guitar Shop</title>
+    <title>My TO DO List</title>
     <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 
 <!-- the body section -->
 <body>
-    <header><h1>Product Manager</h1></header>
+    <header><h1>TO DO Manager</h1></header>
 
     <main>
-        <h1>Add Product</h1>
-        <form action="add_product.php" method="post"
-              id="add_product_form">
+        <h1>Add Task</h1>
+        <form action="add_task.php" method="post"
+              id="add_task_form">
 
-            <label>Category:</label>
-            <select name="category_id">
-            <?php foreach ($categories as $category) : ?>
-                <option value="<?php echo $category['categoryID']; ?>">
-                    <?php echo $category['categoryName']; ?>
+            <label>To Do</label>
+            <select name="todo_id">
+            <?php foreach ($todos as $todo) : ?>
+                <option value="<?php echo $todo['todoID']; ?>">
+                    <?php echo $todo['todoName']; ?>
                 </option>
             <?php endforeach; ?>
             </select><br>
 
-            <label>Code:</label>
-            <input type="text" name="code"><br>
+       
 
             <label>Name:</label>
             <input type="text" name="name"><br>
 
-            <label>List Price:</label>
-            <input type="text" name="price"><br>
+     
 
             <label>&nbsp;</label>
-            <input type="submit" value="Add Product"><br>
+            <input type="submit" value="Add Task"><br>
         </form>
-        <p><a href="index.php">View Product List</a></p>
+        <p><a href="index.php">VIew Task List</a></p>
     </main>
 
     <footer>
-        <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
-    </footer>
+        <p>&copy; <?php echo date("Y"); ?> My To LIst </p>
+    </footer
 </body>
 </html>
